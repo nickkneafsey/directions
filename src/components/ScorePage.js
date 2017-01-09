7337a1baac4a7f3d0382a8497fc999d3c5774b8f
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2017-01-08 18:10:42 -0800

    Added to sagaone and added percentage to scorepage

--- src/components/ScorePage.js
+++ src/components/ScorePage.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ScorePage extends Component {
-  calculateScore() {
-
+  calculateScore(score, total) {
+    return parseInt(parseInt(score) / parseInt(total) * 100)
   }
 
   render() {
+    const { score, total } = this.props;
+
     return (
       <View>
-        <Text>{`Score: ${this.props.score} out of ${this.props.total}`}</Text>
+        <Text>{`Score: ${score} out of ${total}`}</Text>
+        <Text>{`${this.calculateScore(score, total)}%`}</Text>
       </View>
     )
   }
--- src/sagas/SagaOne.js
+++ src/sagas/SagaOne.js
@@ -5,8 +5,18 @@ export default sagaOne = [
     correctAnswer: [ "strawberry" ]
   },
   {
+    emojis: ["hotdog", "taco", "hamburger", "pizza"],
+    directions: "Touch the hotdog",
+    correctAnswer: [ "hotdog" ]
+  },
+  {
     emojis: [ "peach", "watermelon", "strawberry", "pear", "apple", "banana" ],
     directions: "Touch the pear",
     correctAnswer: [ "pear" ]
+  },
+  {
+    emojis: ["hotdog", "taco", "hamburger", "pizza"],
+    directions: "Before you touch the taco, touch the hamburger",
+    correctAnswer: [ "hamburger", "taco" ]
   }
 ];
